9f18931760ef5b597b28543ee3def42f4cd3e009
Seth Krantzler <sethkrantzler@gmail.com>
2025-12-29 12:04:25 -0800

    trying 404 handling

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
@@ -9,4 +14,12 @@ export default defineConfig(({ command }) => ({
     },
   },
   base: command === 'build' ? '/genuary26/' : '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        fallback: path.resolve(__dirname, '404.html'),
+      },
+    },
+  },
 }))
