fec99bfa4097cf79bad9474fccc1bd68ba9e1835
Seth Krantzler <sethkrantzler@gmail.com>
2026-01-03 15:44:02 -0800

    Updates

---
+++ assets/index-ByPlvGZ2.js
@@ -0,0 +1,1 @@
+import{j as e,H as r}from"./main-BNRikTVR.js";const i=()=>e.jsx(r,{position:[0,0,0],center:!0,children:e.jsxs("div",{style:{color:"black",background:"white",padding:"10px",borderRadius:"5px"},children:[e.jsx("h1",{children:"Project for Day 31"}),e.jsx("p",{children:"This is an HTML overlay rendered inside the 3D scene."})]})});export{i as default};
