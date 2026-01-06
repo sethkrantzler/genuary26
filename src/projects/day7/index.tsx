ff530a0fbbaa51ed7e5dba881158f1c17fd3176c
Seth Krantzler <sethkrantzler@gmail.com>
2026-01-06 11:18:21 -0800

    Adding colors

--- src/projects/day7/index.tsx
+++ src/projects/day7/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useMemo } from 'react';
+import * as THREE from 'three';
 import { PromptHint } from '../../components/PromptHint';
 import { CompletedSketch, FullScreenShader } from '../../utils/utils';
 
@@ -6,15 +7,41 @@ type Preset = {
   primaryPetals: number;
   secondaryPetals: number;
   divideSecondary: boolean;
+  paletteColor: THREE.Vector3;
 };
 
 const PRESETS: Preset[] = [
-  { primaryPetals: 2, secondaryPetals: 30, divideSecondary: false },
-  { primaryPetals: 2, secondaryPetals: 2, divideSecondary: false },
-  { primaryPetals: 4, secondaryPetals: 24, divideSecondary: true },
-  { primaryPetals: 12, secondaryPetals: 4, divideSecondary: false },
-  { primaryPetals: 8, secondaryPetals: 12, divideSecondary: true },
-];
+    { 
+      primaryPetals: 2, 
+      secondaryPetals: 30, 
+      divideSecondary: false,
+      paletteColor: new THREE.Vector3(0.0, 0.0, 0.0)
+    },
+    { 
+      primaryPetals: 2, 
+      secondaryPetals: 2, 
+      divideSecondary: false,
+      paletteColor: new THREE.Vector3(0.7, 0.9, 0.2)
+    },
+    { 
+      primaryPetals: 4, 
+      secondaryPetals: 24, 
+      divideSecondary: true,
+      paletteColor: new THREE.Vector3(1.0, 1., 1.)
+    },
+    { 
+      primaryPetals: 12, 
+      secondaryPetals: 4, 
+      divideSecondary: false,
+      paletteColor: new THREE.Vector3(0.2, 0.3, 0.4)
+    },
+    { 
+      primaryPetals: 8, 
+      secondaryPetals: 12, 
+      divideSecondary: true,
+      paletteColor: new THREE.Vector3(1., 0.79, 0.45)
+    },
+  ];
 
 const Day7Project = () => {
     const [currentPresetIndex, setCurrentPresetIndex] = useState(0);
@@ -29,12 +56,14 @@ const Day7Project = () => {
         primaryPetals: { value: currentPreset.primaryPetals },
         secondaryPetals: { value: currentPreset.secondaryPetals },
         divideSecondaryPetals: { value: currentPreset.divideSecondary },
+        paletteColor: { value: currentPreset.paletteColor },
     }), []);
 
     // Update the values when preset changes
     uniforms.primaryPetals.value = currentPreset.primaryPetals;
     uniforms.secondaryPetals.value = currentPreset.secondaryPetals;
     uniforms.divideSecondaryPetals.value = currentPreset.divideSecondary;
+    uniforms.paletteColor.value = currentPreset.paletteColor;
 
     return (
         <>
