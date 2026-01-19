5aeb16cc5abbf10186c78dabef92008645a72821
Seth Krantzler <sethkrantzler@gmail.com>
2026-01-19 00:44:12 -0800

    Updates

---
+++ assets/index-CUIp-d9a.js
@@ -0,0 +1,1 @@
+import{j as e,b0 as r}from"./main-BhDIuBCG.js";const i=()=>e.jsx(r,{position:[0,0,0],center:!0,children:e.jsxs("div",{style:{color:"black",background:"white",padding:"10px",borderRadius:"5px"},children:[e.jsx("h1",{children:"Project for Day 23"}),e.jsx("p",{children:"This is an HTML overlay rendered inside the 3D scene."})]})});export{i as default};
