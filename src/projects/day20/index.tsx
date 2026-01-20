29d88dcc936506da96116ccd5c04219b7a8e6301
Seth Krantzler <sethkrantzler@gmail.com>
2026-01-20 00:27:33 -0800

    final

--- src/projects/day20/index.tsx
+++ src/projects/day20/index.tsx
@@ -2,12 +2,13 @@ import { useTexture } from '@react-three/drei';
 import { PromptHint } from '../../components/PromptHint';
 import { CompletedSketch, FullScreenShader } from '../../utils/utils';
 import * as THREE from 'three';
+import { Bloom, EffectComposer, Glitch, Pixelation } from '@react-three/postprocessing';
 
 const Day20Project = () => {
 
     return (
         <>
-            <PromptHint prompt="a single line" color="grey" />
+            <PromptHint prompt="a single line" hint="drag to break my heart" color="grey" />
             <CompletedSketch day={20} />
             <FullScreenShader
                 fragmentPath={`${import.meta.env.BASE_URL}shaders/day20.glsl`}
