e6ec7ed29167914ea540f4edd74dc43823c5f40b
Seth Krantzler <sethkrantzler@gmail.com>
2026-01-21 13:57:27 -0800

    Updates

---
+++ assets/index-D02K2EKk.js
@@ -0,0 +1,1 @@
+import{j as r,c as t,at as e}from"./main-DMIuvYj_.js";import{P as a}from"./PromptHint-m-MQTcEE.js";const m=()=>r.jsxs(r.Fragment,{children:[r.jsx(a,{prompt:"16x16",hint:"drag to explore",color:"white"}),r.jsx(t,{day:19}),r.jsx(e,{fragmentPath:"/genuary26/shaders/day19.glsl"})]});export{m as default};
