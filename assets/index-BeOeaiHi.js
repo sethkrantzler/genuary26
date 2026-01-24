888624ebf6cbf3828adce736e7c315a39cbbe66b
Seth Krantzler <sethkrantzler@gmail.com>
2026-01-23 18:01:36 -0800

    Updates

---
+++ assets/index-BeOeaiHi.js
@@ -0,0 +1,1 @@
+import{j as r,c as e,at as t}from"./main-I5BWCu1P.js";import{P as a}from"./PromptHint-BpqcOnzU.js";const n=()=>r.jsxs(r.Fragment,{children:[r.jsx(a,{prompt:"a single line",hint:"drag to break my heart",color:"grey"}),r.jsx(e,{day:20}),r.jsx(t,{fragmentPath:"/genuary26/shaders/day20.glsl"})]});export{n as default};
