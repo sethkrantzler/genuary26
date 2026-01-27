38bb5962ba0cff046ee57d327d53cd799e8bb9f9
Seth Krantzler <sethkrantzler@gmail.com>
2026-01-26 23:35:09 -0800

    Updates

---
+++ assets/index-BldS5C3I.js
@@ -0,0 +1,1 @@
+import{j as e,c as r,as as t}from"./main-CmYpCLkq.js";import{P as a}from"./PromptHint-DQlh_stE.js";const n=()=>e.jsxs(e.Fragment,{children:[e.jsx(a,{prompt:"unexpected path following one rule",hint:"drag to change spirograph",color:"white"}),e.jsx(r,{day:18}),e.jsx(t,{fragmentPath:"/genuary26/shaders/day18.glsl"})]});export{n as default};
