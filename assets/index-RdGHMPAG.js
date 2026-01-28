2f0da09ae8d3be256fc07f22a246ef93881ae283
Seth Krantzler <sethkrantzler@gmail.com>
2026-01-28 11:34:30 -0800

    Updates

--- assets/index-RdGHMPAG.js
+++
@@ -1,1 +0,0 @@
-import{j as e,c as r,as as t}from"./main-DWMc-zxm.js";import{P as a}from"./PromptHint-CegiiUGY.js";const n=()=>e.jsxs(e.Fragment,{children:[e.jsx(a,{prompt:"unexpected path following one rule",hint:"drag to change spirograph",color:"white"}),e.jsx(r,{day:18}),e.jsx(t,{fragmentPath:"/genuary26/shaders/day18.glsl"})]});export{n as default};
